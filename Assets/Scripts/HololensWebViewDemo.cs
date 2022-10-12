d8c1a5ba800f3cf31d4d7085647ba5af477af178
Nate Hill <binarynate@gmail.com>
2022-10-11 22:56:49 -0500

    Updated HololensWebViewDemo.cs for 3D WebView v4.3.

--- Assets/Scripts/HololensWebViewDemo.cs
+++ Assets/Scripts/HololensWebViewDemo.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using Vuplex.WebView;
 
@@ -6,24 +5,28 @@ class HololensWebViewDemo : MonoBehaviour {
 
     WebViewPrefab _webViewPrefab;
 
-    void Start() {
+    async void Start() {
 
         // Create a 0.6 x 0.3 instance of the prefab.
+        // https://developer.vuplex.com/webview/WebViewPrefab#Instantiate
         _webViewPrefab = WebViewPrefab.Instantiate(0.6f, 0.3f);
         _webViewPrefab.transform.parent = transform;
         _webViewPrefab.transform.localPosition = new Vector3(0, 0.2f, 1);
         _webViewPrefab.transform.localEulerAngles = new Vector3(0, 180, 0);
-        _webViewPrefab.Initialized += (sender, eventArgs) => {
-            _webViewPrefab.WebView.LoadUrl("https://bing.com");
-        };
 
-        // Add an on-screen keyboard under the main webview.
+        // Add an on-screen keyboard under the webview.
+        // https://developer.vuplex.com/webview/Keyboard
         var keyboard = Keyboard.Instantiate();
         keyboard.transform.parent = _webViewPrefab.transform;
         keyboard.transform.localPosition = new Vector3(0, -0.31f, 0);
         keyboard.transform.localEulerAngles = Vector3.zero;
-        keyboard.InputReceived += (sender, eventArgs) => {
-            _webViewPrefab.WebView.SendKey(eventArgs.Value);
-        };
+
+        // Wait for the prefab to initialize because its WebView property is null until then.
+        // https://developer.vuplex.com/webview/WebViewPrefab#WaitUntilInitialized
+        await _webViewPrefab.WaitUntilInitialized();
+
+        // After the prefab has initialized, you can use the IWebView APIs via its WebView property.
+        // https://developer.vuplex.com/webview/IWebView
+        _webViewPrefab.WebView.LoadUrl("https://bing.com");
     }
 }
